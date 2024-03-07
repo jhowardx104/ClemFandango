2a9c03df8a6219dec82ac6895e465c52e0e3c8fe
jhowardx104 <jhowardx104@gmail.com>
2024-03-07 18:58:17 -0500

    Start destiny 2 data support

---
+++ ClemFandangoBot/ClemFandangoBot/Services/Commands/DestinyModule.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Interactions;
+
+namespace ClemFandangoBot.Services.Commands;
+
+public class DestinyModule: InteractionModuleBase
+{
+    [SlashCommand("nightfall", "Returns details about the current nightfall.")]
+    public async Task Nightfall()
+    {
+        await RespondAsync("The current nightfall is The Devils' Lair. The modifiers are: Arach-NO!, Overload, and Champions: Mob.");
+    }
+}
