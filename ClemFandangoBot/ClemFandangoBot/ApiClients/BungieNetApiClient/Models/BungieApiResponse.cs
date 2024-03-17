d00ebd1bf23fb1c5fb24685114efdd2f76bcea1b
jhowardx104 <jhowardx104@gmail.com>
2024-03-17 11:59:18 -0400

    Add bungie api sample and scratch pad

---
+++ ClemFandangoBot/ClemFandangoBot/ApiClients/BungieNetApiClient/Models/BungieApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ClemFandangoBot.ApiClients.BungieNetApiClient.Models;
+
+public class BungieApiResponse<T>
+{
+    public T Response { get; set; }
+    public int ErrorCode { get; set; }
+    public int ThrottleSeconds { get; set; }
+    public string ErrorStatus { get; set; }
+    public string Message { get; set; }
+    public object MessageData { get; set; }
+}
---
+++ ClemFandangoBot/Scratchpad/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClemFandangoBot.ApiClients.BungieNetApiClient;
+
+var client = new BungieNetApiClient();
+var manifest = await client.GetManifestAsync();
+Console.WriteLine($"Manifest version: {manifest?.Version}");
+Console.WriteLine($"MobileAssetContentPath: {manifest?.MobileAssetContentPath}");
