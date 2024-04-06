60da1ebd23bb2346404d485facbd3bf773a56c78
jhowardx104 <jhowardx104@gmail.com>
2024-04-06 10:29:37 -0400

    start service

---
+++ ClemFandangoBot/ClemFandango.Common/Docker/Services/DockerService.cs
@@ -0,0 +1,8 @@
+﻿using Docker.DotNet;
+
+namespace ClemFandango.Common.Docker.Services;
+
+public class DockerService(IDockerClient client)
+{
+    private readonly IDockerClient _client = client ?? throw new ArgumentNullException(nameof(client));
+}
